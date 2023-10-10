a4ec7984b12e37b35de13342527f2580a5394d3f
AzureAD\고명민 <miung222@gmail.com>
2023-10-10 23:34:13 +0900

    fyne에서 cppExample 패키지 결과 UI로 출력
    GOPATH에 한글 안들어가야 디버깅 실행 가능

--- main.go
+++ main.go
@@ -1,36 +1,39 @@
-// package main
-
-// import (
-// 	"fmt"
-
-// 	"fyne.io/fyne/v2/app"
-// 	"fyne.io/fyne/v2/container"
-// 	"fyne.io/fyne/v2/widget"
-// 	"github.com/Myoungmin/fyneTest/goLib"
-// )
-
-// func main() {
-// 	myApp := app.New()
-// 	myWindow := myApp.NewWindow("Hello")
-
-// 	result := goLib.Add(3, 4) // mygolib 패키지에서 Add 함수 호출
-// 	hello := widget.NewLabel(fmt.Sprintf("Result from C++: %d", result))
-// 	myWindow.SetContent(container.NewVBox(hello, widget.NewButton("Quit", func() {
-// 		myApp.Quit()
-// 	})))
-
-// 	myWindow.ShowAndRun()
-// }
-
 package main
 
 import (
 	"fmt"
 
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+
+	//"github.com/Myoungmin/fyneTest/goLib"
 	"github.com/Myoungmin/fyneTest/cppExample"
 )
 
 func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Hello")
+
+	//result := goLib.Add(3, 4)
 	result := cppExample.Add(3, 4)
-	fmt.Println(result)
+	hello := widget.NewLabel(fmt.Sprintf("Result from C++: %d", result))
+	myWindow.SetContent(container.NewVBox(hello, widget.NewButton("Quit", func() {
+		myApp.Quit()
+	})))
+
+	myWindow.ShowAndRun()
 }
+
+// package main
+
+// import (
+//  "fmt"
+
+//  "github.com/Myoungmin/fyneTest/cppExample"
+// )
+
+// func main() {
+//  result := cppExample.Add(3, 4)
+//  fmt.Println(result)
+// }
